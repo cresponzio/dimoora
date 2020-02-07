85c7fb65864e935e3df55a4bd4306bbc48a09420
Jovisx <suspectgup@gmail.com>
2020-02-08 00:26:45 +1000

    Make richiesta front-end page
    Add richiesta.php file and footer.css.

---
+++ footer.php
@@ -0,0 +1,58 @@
+<footer id="footer">
+    <section style="background: #23262D;" class="group" id="footer">
+        <div style="display: inline-table; width: 270px; margin-top: 30px; margin-left: 22px" id="footer1" onclick="menuAction(this)">
+            <p id="footerTitle">Menu <i class="fas fa-angle-down" style="margin-left: 5px; font-size: 19px; top: 3px; position: relative; display: none;"></i></p>
+            <p style="width: auto; text-align: left; margin: 10px 0px;color: rgba(255, 255, 255, 0.5); line-height: 23px; font-size: 15px; font-weight: 600;">
+                <a class="footer_link" href="https://www.dimoora.it/">Vendi</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/comprare-casa-a-roma">Compra</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/affitta">Affitta</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/agenti-immobiliari">Agenti</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/dimoora">Dimoora</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/chi-siamo">Chi siamo</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/contatti">Contatti</a><br>
+            </p>
+        </div>
+        
+        <div style="display: inline-table; width: 290px; margin-top: 30px;" id="footer2" onclick="menuAction(this)">
+            <p id="footerTitle">Quick Links <i class="fas fa-angle-down" style="margin-left: 5px; font-size: 19px; top: 3px; position: relative; display: none;"></i></p>
+            <p style="width: auto; text-align: left; margin: 10px 0px;color: rgba(255, 255, 255, 0.5); line-height: 23px; font-size: 15px; font-weight: 600;">
+                <a class="footer_link" href="https://www.dimoora.it/comparazione-offerte">Comparazione offerte</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/valutazione-immobile">Valutazione Immobile</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/aumentare-incarichi-con-marketplace/">MARKETPLACE IMMOBILIARE</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/immobili-in-vendita/rm/roma/">Immobili in vendita a Roma</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/immobili-in-vendita/rm/roma/parioli">Immobili in vendita a Parioli</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/immobili-in-vendita/rm/roma/flaminio">Immobili in vendita a Flaminio</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/immobili-in-vendita/rm/roma/portuense">Immobili in vendita a Portuense</a><br>
+            </p>
+        </div>
+        
+        <div style="display: inline-table; width: 150px; margin-top: 30px; margin-left: 20px;" id="footer3" onclick="menuAction(this)">
+            <p id="footerTitle">Blog <i class="fas fa-angle-down" style="margin-left: 5px; font-size: 19px; top: 3px; position: relative; display: none;"></i></p>
+            <p style="width: auto; text-align: left; margin: 10px 0px;color: rgba(255, 255, 255, 0.5); line-height: 23px; font-size: 15px; font-weight: 600;">
+                <a class="footer_link" href="https://www.dimoora.it/il-blog-dimoora">Tutti gli articoli</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/blog/consigli-immobiliari/zone/">Zone</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/blog/consigli-immobiliari/vendita/">Vendita</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/blog/consigli-immobiliari/affitto/">Affitto</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/blog/consigli-per-agenti-immobiliari/mercato-immobiliare/">Mercato immobiliare</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/blog/consigli-per-agenti-immobiliari/formazione/">Formazione</a><br>
+                <a class="footer_link" href="https://www.dimoora.it/blog/consigli-per-agenti-immobiliari/casi-di-studio/">Casi di studio</a><br>
+            </p>
+        </div>
+        
+        <div style="display: inline-table; width: 280px; text-align: left; border-right: 1px solid rgba(255, 255, 255, 0.5); padding-right: 30px;margin-right: 40px; margin-top: 30px;margin-left: 40px; float: left" id="footer0">
+            <img src="img/logo.png" id="logo" style="height: 22px;">
+            <p style="width: auto; text-align: left; margin: 30px 0px;color: rgba(255, 255, 255, 0.5); line-height: 23px; font-size: 15px; font-weight: 600;">Viale Liegi, 35/b<br>00198 - Roma, Italia<br>Email: segreteria@dimoora.it<br>Telefono: +39 06 8552 053<br>P.IVA 15524401</p>
+        </div>
+        
+        <div style="width: 250px; margin-left: 412px; padding: 30px;" id="footerBottom">
+            <div style="position:relative; left: -410px;" id="i">
+                <a href="https://www.instagram.com/dimoora.it/"><i class="fab fa-instagram" style="font-size: 21px; color: #fff; margin-right: 13px"></i></a>
+                <a href="https://www.facebook.com/Dimoora-343384516318451/"><i class="fab fa-facebook-square" style="font-size: 21px; color: #fff; margin-right: 13px"></i></a>
+                <a href="https://www.linkedin.com/company/dimoora/"><i class="fab fa-linkedin" style="font-size: 21px; color: #fff; margin-right: 13px"></i></a>
+            </div>
+            
+            <p style="color: rgba(255, 255, 255, 0.6); font-size: 13px; width: 100%; text-align: left; margin-top: 0px; top: -20px; position:relative">© Dimoora.it 2018-2019</p>
+            <p style="width: auto;color: rgba(255, 255, 255, 0.6); font-size: 16px; text-align: left; opacity: 1;">Developed by : <img src="img/logo.png" style="height: 15px; margin-left: 10px; top: 3px; position:relative;"></p>
+        </div>
+    </section>
+</footer>
